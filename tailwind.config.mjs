3a5f8227ac9f41c0b0e26a9f5e31264f4dfd63c4
Emily Nielsen <emily.nielsen09@gmail.com>
2025-05-06 11:47:56 +0200

    designsystem done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,127 +1,234 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 
-    /* Vil du overskrive eller extende tailwinds indstillinger?  */
+  /* Vil du overskrive eller extende tailwinds indstillinger?  */
 
-	theme: {
-  current: 'currentColor',
-         'beige': {
-             50: '#hexkode',     
-                 100: '#hexkode',
-                 200: '#hexkode',
-                 300: '#hexkode',
-                 400: '#hexkode',
-                 500: '#hexkode',
-                 600: '#hexkode',
-       
-           },
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
 
-           'green': {
-            50: '#hexkode',     
-                100: '#hexkode',
-                200: '#hexkode',
-                300: '#hexkode',
-                400: '#hexkode',
-                500: '#hexkode',
-                600: '#hexkode',
-      
-          },
-          'red': {
-            50: '#hexkode',     
-                100: '#hexkode',
-                200: '#hexkode',
-                300: '#hexkode',
-                400: '#hexkode',
-                500: '#hexkode',
-                600: '#hexkode',
-      
-          },
-       
-		extend: {
+        beige: {
+          50: "#fdfdfc",
+          500: "#efeae3",
+          300: "#f7f3ef",
+        },
 
-            opacity:{
-                50: '.50',   
-   
-            },
+        green: {
+          500: "#0d3d2b",
+          900: "#030d09",
+        },
 
-         /* Tilføj din egen spacing nedenfor */
-        spacing: {
-            'xxs': '10px',
-            'xs':'20px',
-            's':'40px',
-            'm':'80px',
-            'l':'120px',
+        red: {
+          500: "#ce3732",
+          700: "#7b211e",
+          
+        },
+      },
+
+      opacity:{
+        40: '.40', 
+        60: '.60', 
+        80: '.80',   
+
+    },
+
+
+      spacing: {
+        spacebard136: "136px",
+        spacebar32: "32px",
+        spacebarm64: "64px",
+        spacebarm16: "16px",
       },
 
       /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
       fontFamily: {
-        dmsans: ['DM sans', 'sans-serif'],
-        gloock: ['Gloock', 'serif'],
-        
+        dmsans: ["DM sans", "sans-serif"],
+        gloock: ["Gloock", "serif"],
       },
 
-      fontWeight:{
-         regular:'400',
-         semibold:'600',
-         bold:'700',
-
+      fontWeight: {
+        light:"300",
+        regular: "400",
+        medium:"500",
+        semibold: "600",
+        bold: "700",
       },
 
-      borderWidth:{
-         DEFAULT:'2px',
-
+      borderWidth: {
+        DEFAULT: "1px",
       },
 
-      dropShadow:{
-         regular:'5px 4px 4px rgba(0,0, 0.25)'
-         
-      },
+    
 
-         /* Tilføj dit typografiske hierarki herunder */
-     fontSize: {
-       'h1-mobil': ['89.8px', { lineHeight: 'auto' }],
-         'h2-mobil': ['67.3px', { lineHeight: 'auto' }],
-         'h3-mobil': ['50.5px', { lineHeight: 'auto' }],
-         'h4-mobil': ['37.9px', { lineHeight: 'auto' }],
-         'h5-mobil': ['28.4px', { lineHeight: 'auto' }],
-         'h6-mobil': ['21.3px', { lineHeight: 'auto' }],
-         'p-mobil': ['16px', { lineHeight: 'auto' }],
-         'h1-display-desktop': ['150px', { lineHeight: '120px' }],
-         'h1-desktop': ['112.2px', { lineHeight: '112.2px' }],
-         'h2-desktop': ['84.2px', { lineHeight: 'auto' }],
-         'h3-desktop': ['63.2px', { lineHeight: 'auto' }],
-         'h4-desktop': ['47.4px', { lineHeight: 'auto' }],
-         'h5-desktop': ['35.5px', { lineHeight: 'auto' }],
-         'h6-desktop': ['26.7px', { lineHeight: 'auto' }],
-         'p-desktop': ['20px', { lineHeight: 'auto', fontWeight:'700', fontFamily:'dmsans' }],
-        
+      fontSize: {
+        "h1-mobil": [
+          "40px",
+          { lineHeight: "120", fontWeight: "400", fontFamily: "gloock", textTransform: 'uppercase' },
+        ],
+        "h2-mobil": [
+          "20px",
+          { lineHeight: "120", fontWeight: "400", fontFamily: "gloock", textTransform: 'uppercase' },
+        ],
+        "h3-mobil": [
+          "15px",
+          { lineHeight: "39", fontWeight: "400", fontFamily: "dmsans", textTransform: 'uppercase' },
+        ],
+        "h4-mobil": [
+          "15px",
+          { lineHeight: "39", fontWeight: "400", fontFamily: "dmsans", textTransform: 'lowercase' },
+        ],
+        "h5-mobil": [
+          "15px",
+          { lineHeight: "31", fontWeight: "700", fontFamily: "dmsans", textTransform: 'uppercase' },
+        ],
+        "h6-mobil": [
+          "15px",
+          { lineHeight: "31", fontWeight: "700", fontFamily: "dmsans", textTransform: 'lowercase' },
+        ],
+"h7-mobil": [
+          "12px",
+          { lineHeight: "31", fontWeight: "400", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "p-regular-mobil": [
+          "20px",
+          { lineHeight: "140%", fontWeight: "400", fontFamily: "dmsans", textTransform: '' },
+        ],
+"p-small-mobil": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "400", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "caption-mobil": [
+          "12px",
+          { lineHeight: "140%", fontWeight: "300", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "label-mobil": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "700", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "input-mobil": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "500", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "link-mobil": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "700", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "button-primary-mobil": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "700", fontFamily: "dmsans", textTransform: 'uppercase' },
+        ],
+
+        "link-menu-mobil": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "700", fontFamily: "dmsans", textTransform: 'uppercase' },
+        ],
+
+
+
+
+        "h1-desktop": [
+          "130px",
+          { lineHeight: "120", fontWeight: "400", fontFamily: "gloock", textTransform: 'uppercase' },
+        ],
+        "h2-desktop": [
+          "56px",
+          { lineHeight: "120", fontWeight: "400", fontFamily: "gloock", textTransform: 'uppercase' },
+        ],
+        "h3-desktop": [
+          "30px",
+          { lineHeight: "39", fontWeight: "400", fontFamily: "dmsans", textTransform: 'uppercase' },
+        ],
+        "h4-desktop": [
+          "30px",
+          { lineHeight: "39", fontWeight: "400", fontFamily: "dmsans", textTransform: 'lowercase' },
+        ],
+        "h5-desktop": [
+          "24px",
+          { lineHeight: "31", fontWeight: "700", fontFamily: "dmsans", textTransform: 'uppercase' },
+        ],
+        "h6-desktop": [
+          "24px",
+          { lineHeight: "31", fontWeight: "700", fontFamily: "dmsans", textTransform: 'lowercase' },
+        ],
+"h7-desktop": [
+          "24px",
+          { lineHeight: "31", fontWeight: "400", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "p-regular-desktop": [
+          "20px",
+          { lineHeight: "140%", fontWeight: "400", fontFamily: "dmsans", textTransform: '' },
+        ],
+"p-small-desktop": [
+          "20px",
+          { lineHeight: "140%", fontWeight: "400", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "caption-desktop": [
+          "14px",
+          { lineHeight: "140%", fontWeight: "300", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "label-desktop": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "700", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "input-desktop": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "500", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "link-desktop": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "700", fontFamily: "dmsans", textTransform: '' },
+        ],
+
+        "button-primary-desktop": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "700", fontFamily: "dmsans", textTransform: 'uppercase' },
+        ],
+
+        "link-menu-desktop": [
+          "16px",
+          { lineHeight: "140%", fontWeight: "700", fontFamily: "dmsans", textTransform: 'uppercase' },
+        ],
+
+       
       },
 
       gridTemplateColumns: {
-        'grid-mobil': 'repeat(4, 1fr)',
-        'grid-tablet':'repeat (8, 1fr)',
-        'grid-desktop': 'repeat(12, 1fr)',
-
+        "grid-mobil": "repeat(12, 1fr)",
+        "grid-tablet": "repeat (12, 1fr)",
+        "grid-desktop": "repeat(12, 1fr)",
       },
 
-   margin:{
-    'margin-mobil': '20px',
-    'margin-tablet': '40px',
-    'margin-desktop': '60px',
-},
+      margin: {
+        "margin-mobil": "16px",
+        "margin-tablet": "32px",
+        "margin-desktop": "64px",
+      },
 
-gap:{
-    'gap-mobil': '20px',
-    'gap-tablet': '30px',
-    'gap-desktop': '40px',
-},
-        },
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+      gap: {
+        "gap-mobil": "4px",
+        "gap-tablet": "6px",
+        "gap-desktop": "8px",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
